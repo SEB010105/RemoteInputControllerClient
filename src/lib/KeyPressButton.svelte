f46dde51589ecac829ade1cd8e97fc2020f959af
Sebastian <s.huber@htlstp.at>
2024-01-22 20:59:16 +0100

    removed default content + added test button

---
+++ src/lib/KeyPressButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+    export let content:string;
+
+    function handleClick() {
+        console.log(content)
+    }
+</script>
+
+<div>
+    <button
+            on:click={handleClick}
+    >{content}</button>
+</div>
